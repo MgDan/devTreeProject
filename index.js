8bb1ac1d15a4563d4031dfb4ecdc3a4c315e4eec
Daniel Vázquez <danielvzzesc@gmail.com>
2025-11-16 11:29:45 -0600

    [dTP]-001/feat: agregado la instancia y el objeto servidor de express

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log('Hola mundo desde devtree')
+
+//en node la sintaxis por defult es commonjs
+const express = require('express')
+
+/*
+* para ver que este funcionando
+console.log(express);
+*/
+
+
+
+//crea una instancia de servidor y almacena el objeto en la variable app
+const app = express();
+
+app.get('/',(req, res)=>{
+    res.send('Hola mundo en Express')
+})
+
+app.get('/ecom',(req, res)=>{
+    res.send('Este es el ecommerce')
+})
+
+// para crear el servidor
+app.listen(4000, ()=>{ 
+    console.log('Servidor funcionando')
+})
+/** 
+ * 
+ * esperado ver en el navegador "cannot GET /" 
+ * no se ha especificado que hay que mostrar en esa url
+ * 
+ * 
+ * necesitas agregar el routing 
+ * 
+ * app.get('/',(req, res)=>{
+ * 
+ * })
+ */
